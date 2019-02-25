3a9f3ff7c11a07971053985c010b537da9bb180b
Artsem Stalavitski <a.stalavitski@gmail.com>
2019-02-25 12:43:16 +0300

    Templates & Placeholders

--- mysite/settings.py
+++ mysite/settings.py
@@ -165,7 +165,6 @@ CMS_LANGUAGES = {
 }
 
 CMS_TEMPLATES = (
-    ## Customize this
     ('fullwidth.html', 'Fullwidth'),
     ('sidebar_left.html', 'Sidebar Left'),
     ('sidebar_right.html', 'Sidebar Right')
